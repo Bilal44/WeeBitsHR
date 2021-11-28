4ccb7526abf3d3b0d82df37a33165955c40d71f6
Bilal <bilalahmed90@yahoo.com>
2021-11-28 03:19:36 +0000

    Add annual staff turnover ratio, all user stories complete

--- WeeBitsHRService/Controllers/ReportsController.cs
+++ WeeBitsHRService/Controllers/ReportsController.cs
@@ -39,39 +39,79 @@ namespace WeeBitsHRService.Controllers
                 && (!endAge.HasValue || e.DOB > DateTime.Now.AddYears(-endAge.Value))
                 && (!gender.HasValue || e.Gender == gender)
                 && e.IsActive)
-                .Include(e => e.JobCategory).Include(e => e.Branch).ToListAsync();
+                .Include(e => e.JobCategory).ToListAsync();
 
             ViewData["Count"] = employees.Count;
-            
+            ViewData["StartAge"] = startAge;
+            ViewData["EndAge"] = endAge;
+            ViewData["Gender"] = gender;
+
             return View(employees);
         }
 
         // GET: AveragePayrate
-        public async Task<IActionResult> AveragePayrate(Gender? gender, int? branchId)
+        public async Task<IActionResult> AveragePayrate(int? branchId)
         {
             var employees = await _context.Employees
                 .Where(e => (!branchId.HasValue || e.BranchId == branchId)
-                && (!gender.HasValue || e.Gender == gender)
                 && e.IsActive)
-                .Include(e => e.JobCategory).Include(e => e.Branch).ToListAsync();
+                .Include(e => e.JobCategory).ToListAsync();
 
             ViewData["AvgPaybyGender"] = employees.GroupBy(e => e.Gender).Select(g => new AveragePayByGenderViewModel { Gender = g.Key, AveragePay = g.Average(e => e.Salary) });
             ViewData["AvgPaybyJob"] = employees.GroupBy(e => e.JobCategory).Select(g => new AveragePayByJobViewModel { JobCategory = g.Key, AveragePay = g.Average(e => e.Salary) });
-
+            ViewData["Branch"] = branchId != null || branchId != 0 ? _context.Branches.FirstOrDefault(b => b.Id == branchId)?.Region : "";
 
             return View(employees);
         }
 
         // GET: AbsenteeProfile
-        public async Task<IActionResult> AbsenteeProfile()
+        public async Task<IActionResult> AbsenteeProfile(DateTime? startDate, DateTime? endDate)
         {
-            var employees = await _context.Employees
-                .Where(e => e.IsActive)
-                .Include(e => e.JobCategory).Include(e => e.Branch).Include(e => e.Absences).ToListAsync();
+            var employees = _context.Employees
+                .Include(e => e.JobCategory).Where(e => e.IsActive).Include(e => e.Absences
+                .Where(a=> !startDate.HasValue || a.DateofAbsence >= startDate
+                && (!endDate.HasValue || a.DateofAbsence <= endDate))).ToList();
 
             var absenteeProfile = employees.GroupBy(e => e.JobCategory).Select(g => new AbsenteeProfileViewModel { JobCategory = g.Key, AverageHoursOfAbsence = g.Average(e => e.Absences.Sum(a => a.NumberOfHours)), TotalEmployees = g.Count() });
-            
+
+            ViewData["StartDate"] = startDate.Value.ToShortDateString();
+            ViewData["EndDate"] = endDate.Value.ToShortDateString();
+
             return View(absenteeProfile);
+        }
+
+        // GET: AnnualStaffTurnOver
+        public async Task<IActionResult> AnnualStaffTurnover(int? branchId, int? year)
+        {
+            var model = new AnnualTurnoverViewModel();
+            if (year == null || Convert.ToInt32(year) == 0)
+            {
+                return View(model);
+            }
+
+            var initialEmployees = await _context.Employees
+                .Where(e => (!branchId.HasValue || e.BranchId == branchId)
+                && e.JoinDate.Year < year
+                && e.IsActive)
+                .Include(e => e.Branch).CountAsync();
+
+            var newEmployees = await _context.Employees
+                .Where(e => (!branchId.HasValue || e.BranchId == branchId)
+                && e.JoinDate.Year == year
+                && e.IsActive)
+                .Include(e => e.Branch).CountAsync();
+
+            var employeesLeft = await _context.Employees
+                .Where(e => (!branchId.HasValue || e.BranchId == branchId)
+                && e.LeaveDate.Value.Year == year)
+                .Include(e => e.Branch).CountAsync();
+
+            var averageEmployees = (initialEmployees * 2 + newEmployees - employeesLeft) / 2;
+            var annualStaffTurnover = averageEmployees > 0 ? employeesLeft * 100 / averageEmployees : 0;
+
+            var branch = _context.Branches.FirstOrDefault(b => b.Id == branchId)?.Region;
+            model = new AnnualTurnoverViewModel(averageEmployees, newEmployees, employeesLeft, annualStaffTurnover, year.Value, branch);
+            return View(model);
         }
     }
 }
---
+++ WeeBitsHRService/ViewModels/AnnualTurnoverViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WeeBitsHRService.Models;
+
+namespace WeeBitsHRService.ViewModels
+{
+    public class AnnualTurnoverViewModel
+    {
+        [Display(Name = "Average Number of Employees")]
+        public int AverageNumberOfEmployees { get; set; }
+
+        [Display(Name = "New Employees")]
+        public int NewEmployees { get; set; }
+
+        [Display(Name = "Employees Left")]
+        public int EmployeesLeft { get; set; }
+
+        [Display(Name = "Annual Staff Turnover Ratio")]
+        public double StaffTurnoverRatio { get; set; }
+
+        public int Year { get; set; }
+        public String Region { get; set; }
+
+        public AnnualTurnoverViewModel(int averageNumberOfEmployees, int newEmployees, int employeesLeft, double staffTurnoverRatio, int year, string region)
+        {
+            AverageNumberOfEmployees = averageNumberOfEmployees;
+            NewEmployees = newEmployees;
+            EmployeesLeft = employeesLeft;
+            StaffTurnoverRatio = staffTurnoverRatio;
+            Year = year;
+            Region = region;
+        }
+
+        public AnnualTurnoverViewModel()
+        {
+        }
+    }
+}
