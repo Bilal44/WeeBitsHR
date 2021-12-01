541621daf23f040f0e54bcdfa36ee02952eac4eb
Bilal <bilalahmed90@yahoo.com>
2021-12-01 08:53:50 +0000

    Improve layout and validate unused models, Move README

--- WeeBitsHRService/Models/Branch.cs
+++ WeeBitsHRService/Models/Branch.cs
@@ -1,16 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WeeBitsHRService.Models
 {
 	public class Branch
 	{
 		public int Id { get; set; }
+
+		[Required]
 		public string Name { get; set; }
+
+		[Required]
 		public string AddressLine1 { get; set; }
+
 		public string? AddressLine2 { get; set; }
+
+		[Required]
 		public string Town { get; set; }
+
+		[Required]
 		public string PostCode { get; set; }
+
+		[Required]
 		public string Country { get; set; }
+
+		[Required]
 		public string PhoneNumber { get; set; }
 		[NotMapped]
 		public string Region { get => Name + ", " + Country; }
--- WeeBitsHRService/Models/JobCategory.cs
+++ WeeBitsHRService/Models/JobCategory.cs
@@ -1,8 +1,12 @@
-﻿namespace WeeBitsHRService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeeBitsHRService.Models
 {
 	public class JobCategory
 	{
 		public int Id { get; set; }
+
+		[Required]
 		public string Name { get; set; }
 	}
 }
