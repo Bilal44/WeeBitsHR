4677cc2df2dd83e8b6c5adedf8b91fbf7eb8d5ab
Bilal <bilalahmed90@yahoo.com>
2021-12-16 22:28:31 +0000

    Delete unneeded pages, Streamline formatting for manage account pages

--- WeeBitsHRService/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ WeeBitsHRService/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -22,37 +22,7 @@ namespace WeeBitsHRService.Areas.Identity.Pages.Account.Manage
 		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
 		///     directly from your code. This API may change or be removed in future releases.
 		/// </summary>
-		public static string Email => "Email";
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
 		public static string ChangePassword => "ChangePassword";
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public static string DownloadPersonalData => "DownloadPersonalData";
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public static string DeletePersonalData => "DeletePersonalData";
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public static string ExternalLogins => "ExternalLogins";
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public static string PersonalData => "PersonalData";
 
 		/// <summary>
 		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -70,37 +40,7 @@ namespace WeeBitsHRService.Areas.Identity.Pages.Account.Manage
 		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
 		///     directly from your code. This API may change or be removed in future releases.
 		/// </summary>
-		public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
 		public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public static string DownloadPersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DownloadPersonalData);
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public static string DeletePersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DeletePersonalData);
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
 
 		/// <summary>
 		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -115,7 +55,7 @@ namespace WeeBitsHRService.Areas.Identity.Pages.Account.Manage
 		public static string PageNavClass(ViewContext viewContext, string page)
 		{
 			var activePage = viewContext.ViewData["ActivePage"] as string
-				?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+				?? Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
 			return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
 		}
 	}
