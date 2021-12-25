51a87674e41c68ee9eb45de8bab1d3a874e5ddd1
Bilal <bilalahmed90@yahoo.com>
2021-12-25 15:24:28 +0000

    Add employee profile page displaying all their details

--- WeeBitsHRService/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ WeeBitsHRService/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -2,83 +2,42 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using WeeBitsHRService.Data;
+using WeeBitsHRService.Models;
 
 namespace WeeBitsHRService.Areas.Identity.Pages.Account.Manage
 {
 	public class IndexModel : PageModel
 	{
-		private readonly UserManager<IdentityUser> _userManager;
-		private readonly SignInManager<IdentityUser> _signInManager;
+		private readonly ApplicationDbContext _context;
 
-		public IndexModel(
-			UserManager<IdentityUser> userManager,
-			SignInManager<IdentityUser> signInManager)
+		public IndexModel(ApplicationDbContext context)
 		{
-			_userManager = userManager;
-			_signInManager = signInManager;
+			_context = context;
 		}
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public string Username { get; set; }
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		[TempData]
-		public string StatusMessage { get; set; }
 
 		/// <summary>
 		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
 		///     directly from your code. This API may change or be removed in future releases.
 		/// </summary>
 		[BindProperty]
-		public InputModel Input { get; set; }
-
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public class InputModel
-		{
-			/// <summary>
-			///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-			///     directly from your code. This API may change or be removed in future releases.
-			/// </summary>
-			[Phone]
-			[Display(Name = "Phone number")]
-			public string PhoneNumber { get; set; }
-		}
-
-		private async Task LoadAsync(IdentityUser user)
-		{
-			var userName = await _userManager.GetUserNameAsync(user);
-			var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-
-			Username = userName;
-
-			Input = new InputModel
-			{
-				PhoneNumber = phoneNumber
-			};
-		}
+		public Employee Model { get; set; }
 
 		public async Task<IActionResult> OnGetAsync()
 		{
-			var user = await _userManager.GetUserAsync(User);
-			if (user == null)
+			Model = await _context.Employees
+				.Where(e => e.Email == User.Identity.Name)
+				.Include(e => e.Branch).Include(e => e.JobCategory)
+				.FirstOrDefaultAsync();
+
+			if (Model == null)
 			{
-				return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+				return NotFound($"Unable to load user with email '{User.Identity.Name}'.");
 			}
 
-			await LoadAsync(user);
 			return Page();
 		}
 	}
